67e54a5618eaa3a580adc1228a8e52a882b9e667
Gobinath B <princegopinath42@gmail.com>
2024-04-20 18:24:29 +0530

    chore

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Router from "./routes";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store} />
+      <Router />
+    </div>
+  );
+}
+
+export default App;
