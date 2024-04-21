e904e81bdf0dd57c65b7fc8e67ce9c91d3bc87c4
Gobinath B <princegopinath42@gmail.com>
2024-04-21 13:05:06 +0530

    refactor authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../container/Home';
-import { AuthProvider } from './authProvider';
-import Login from '../container/login';
-import Register from '../container/register';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../container/Home";
+import { AuthProvider } from "./authProvider";
+import Login from "../container/login";
+import Register from "../container/register";
+import AuthMiddleware from "./authMiddileware";
 
 export default function Router() {
-
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path='*' element={<Home />} />
+
+          <Route
+                        path="*"
+                        element={
+                            <AuthMiddleware>
+                                <Home />
+                            </AuthMiddleware>
+                        }
+                    />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
